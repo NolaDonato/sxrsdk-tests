d8cc5bd17e1befff943346df54048b1895941871
NolaDonato <nola.donato@samsung.com>
2019-02-13 21:34:10 -0800

    bug fix to mesh tests
    designate transparent render order

--- framework-tests/app/src/main/cpp/helpers.cpp
+++ framework-tests/app/src/main/cpp/helpers.cpp
@@ -3,7 +3,7 @@
 extern "C" {
 
 JNIEXPORT jlongArray JNICALL
-Java_org_gearvrf_tester_StateSortTests_getRenderDataVector(JNIEnv *env, jclass type) {
+Java_com_samsungxr_tester_StateSortTests_getRenderDataVector(JNIEnv *env, jclass type) {
     jlongArray result = env->NewLongArray(1);
 
     return result;
